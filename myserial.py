7a1e81f2afcaf413546cf5c494a36f9b088631d4
YasinRezaei <109751572+yasin-rezaei@users.noreply.github.com>
2025-10-18 16:54:27 +0330

    Update myserial.py

--- myserial.py
+++ myserial.py
@@ -1,1 +1,11 @@
 
+import serial
+from time import sleep
+
+def send(data):
+      data = data+"\n"
+      ser.write(data.encode('utf-8'))
+    
+if __name__ == 'myserial':
+    ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
+    ser.reset_input_buffer()
